aea3819adaa5423d66c4a1f6add637859fe710f8
bangundwir <hendrabangundwir@gmail.com>
2020-12-15 13:16:59 +0700

    halo

--- logs/log-2020-12-15.php
+++ logs/log-2020-12-15.php
@@ -45,3 +45,48 @@ ERROR - 2020-12-15 12:47:15 --> Array
     [total_time_us] => 361100
 )
 
+ERROR - 2020-12-15 13:10:52 --> Curl error: SSL certificate problem: unable to get local issuer certificate
+ERROR - 2020-12-15 13:10:52 --> Array
+(
+    [url] => https://pantau.opensid.my.id/index.php/api/track/desa?token=da39a3ee5e6b4b0d3255bfef95601890afd80709
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 0
+    [filetime] => -1
+    [ssl_verify_result] => 20
+    [redirect_count] => 0
+    [total_time] => 0.669292
+    [namelookup_time] => 0.002053
+    [connect_time] => 0.366359
+    [pretransfer_time] => 0
+    [size_upload] => 0
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 0
+    [download_content_length] => -1
+    [upload_content_length] => -1
+    [starttransfer_time] => 0
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 103.146.203.49
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.1.209
+    [local_port] => 5860
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 0
+    [connect_time_us] => 366359
+    [namelookup_time_us] => 2053
+    [pretransfer_time_us] => 0
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 0
+    [total_time_us] => 669292
+)
+
--- themes/natra/partials/footer_bottom.php
+++ themes/natra/partials/footer_bottom.php
@@ -3,28 +3,29 @@
       <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
           <div class="footer_bottom_left">
-              <?php if (file_exists('mitra')): ?>
+            <?php if (file_exists('mitra')) : ?>
               Hosting didukung <a href="https://my.idcloudhost.com/aff.php?aff=3172" rel="noopener noreferrer" target="_blank">
-                  <img src="<?= base_url('/assets/images/Logo-IDcloudhost.png')?>" height='15px' alt="Logo-IDCloudHost" title="Logo-IDCloudHost"></a>
-              <?php endif; ?>
+                <img src="<?= base_url('/assets/images/Logo-IDcloudhost.png') ?>" height='15px' alt="Logo-IDCloudHost" title="Logo-IDCloudHost"></a>
+            <?php endif; ?>
           </div>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
           <div class="footer_bottom_right">
-		   &copy; <a href="https://opendesa.id/" rel="noopener noreferrer" target="_blank">OpenDesa</a>
-		   <i class="fa fa-circle" style="font-size: smaller;"></i>
-		   <a href="https://github.com/OpenSID/OpenSID" rel="noopener noreferrer" target="_blank">OpenSID <?= AmbilVersi()?></a>
-		   <a href="<?= site_url(); ?>siteman" rel="noopener noreferrer" target="_blank"> | Natra 4.3</a>
+            &copy; <a href=http://desa-toyomarto.malangkab.go.id/desa/" rel="noopener noreferrer" target="_blank">Toyomarto</a>
+            <i class="fa fa-circle" style="font-size: smaller;"></i>
+            <a href="http://desa-toyomarto.malangkab.go.id/desa/" rel="noopener noreferrer" target="_blank">toyomarto <?= AmbilVersi() ?></a>
+            <a href="<?= site_url(); ?>siteman" rel="noopener noreferrer" target="_blank"> | freedom</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-<script type="text/javascript">
-function printDiv(divName) {
-	var printContents = document.getElementById(divName).innerHTML;
-	var originalContents = document.body.innerHTML;
-	document.body.innerHTML = printContents;
-	window.print();
-	document.body.innerHTML = originalContents;
-} </script>
+  <script type="text/javascript">
+    function printDiv(divName) {
+      var printContents = document.getElementById(divName).innerHTML;
+      var originalContents = document.body.innerHTML;
+      document.body.innerHTML = printContents;
+      window.print();
+      document.body.innerHTML = originalContents;
+    }
+  </script>
